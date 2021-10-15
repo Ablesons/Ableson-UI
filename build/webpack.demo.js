08f7aa51d35fd467e1adc3ade21a34bdbeb55ded
Ablesons <huangzhendong1996@outlook.com>
2021-10-15 16:22:50 +0800

    chore(package.json): 依赖更新

--- build/webpack.demo.js
+++ build/webpack.demo.js
@@ -10,61 +10,60 @@ const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: "./examples/entry.js",
-    output: {
-        path: path.resolve(process.cwd(), './examples/ableson-ui/'),
-        filename: "bundle.js"
-    },
-    devServer: {
-        host: 'localhost',
-        port: 8080,
-        publicPath: '/',
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(jsx?|babel|es6)$/,
-                include: process.cwd(),
-                exclude: /node_modules/,
-                // exclude: config.jsexclude,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    compilerOptions: {
-                        preserveWhitespace: false
-                    }
-                }
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(svg|otf|ttf|woff2?|eot|gif|png|jpe?g)(\?\S*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    esModule: false
-                }
-            }
+  mode: process.env.NODE_ENV,
+  entry: "./examples/entry.js",
+  output: {
+    path: path.resolve(process.cwd(), './examples/ableson-ui/'),
+    filename: "bundle.js"
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    publicPath: '/',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?|babel|es6)$/,
+        include: process.cwd(),
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            preserveWhitespace: false
+          }
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            favicon: "./examples/favicon.ico",
-            template: "./examples/index.html",
-            filename: "index.html",
-            inject: true
-        }),
+      },
+      {
+        test: /\.(svg|otf|ttf|woff2?|eot|gif|png|jpe?g)(\?\S*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          esModule: false
+        }
+      }
     ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      favicon: "./examples/favicon.ico",
+      template: "./examples/index.html",
+      filename: "index.html",
+      inject: true
+    })
+  ]
 };
